2ebce08ce2c4cf99ceb305bec1bb337d90f033c3
iMythms <maythams.149@gmail.com>
2024-11-12 18:31:14 +0300

    initial commit: set up user authentication with sign-up and sign-in functionality, including session management and middleware

---
+++ controller/auth.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+
+router.get('/sign-up', (req, res) => {
+	res.render('auth/sign-up.ejs')
+})
+
+const signedOut = (req, res) => {
+	req.session.destroy(() => {
+		res.redirect('/')
+	})
+}
+
+router.get('sign-out', signedOut)
+
+router.post('/sign-up', async (req, res) => {
+	const userInDatabase = await User.findOne({ username: req.body.username })
+	if (userInDatabase) {
+		return res.send('Username already taken')
+	}
+	if (req.body.password !== req.body.confirmPassword) {
+		return res.send('Password and Confirm Password must match')
+	}
+
+	const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+	res.body.password = hashedPassword
+
+	// save the user
+	const user = await User.create(req.body)
+
+	req.session.user = {
+		username: user.username,
+		_id: user._id,
+	}
+	req.session.save(() => {
+		res.redirect('/')
+	})
+})
+
+router.get('/sign-in', (req, res) => {
+	res.render('auth/sign-in.ejs')
+})
+
+router.post('/sign-in', async (req, res) => {
+	const userInDatabase = await User.findOne({ username: req.body.username })
+	if (!userInDatabase) {
+		return res.send('Login failed. Please try again.')
+	}
+
+	req.session.user = {
+		username: userInDatabase.username,
+		_id: userInDatabase._id,
+	}
+
+	req.session.save(() => {
+		res.redirect('/')
+	})
+})
+
+module.exports = router
